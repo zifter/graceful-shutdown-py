08dbc663c8e0fa77c81aade6fdd4d9b32f73a39e
Aleh Strakachuk <o.strokachuk@strikerz.inc>
2023-01-16 21:22:49 +0200

    fix of tox pipelines

--- src/tests/gracefull_shutdown_test.py
+++ src/tests/gracefull_shutdown_test.py
@@ -15,3 +15,16 @@ def test_trigger_ok():
     shutdown.trigger()
 
     assert shutdown.triggered is True
+
+
+def test_cast_to_bool_ok():
+    """
+    Check correctness of casting to bool
+    """
+    shutdown = ExitSignalHandler()
+
+    assert bool(shutdown) is False
+
+    shutdown.trigger()
+
+    assert bool(shutdown) is True
